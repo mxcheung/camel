2c8a827016376ef8e2c4517d0cb302216611b076
max cheung <maxcheung@optusnet.com.au>
2018-07-18 23:28:59 +1000

    Route def

--- datatransfer/src/main/java/com/baeldung/camel/route/MyRouteBuilder.java
+++ datatransfer/src/main/java/com/baeldung/camel/route/MyRouteBuilder.java
@@ -2,11 +2,15 @@ package com.baeldung.camel.route;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.lang3.StringUtils;
 
 import com.baeldung.camel.model.RouteDef;
 
 public class MyRouteBuilder extends RouteBuilder {
 
+	private static final String DEFAULT_TRACING = "true";
+	private static final String DEFAULT_BODY = ">>> ${body}";
+	
 	private final String routeId;
 	private final String from;
 	private final String destination;
@@ -18,8 +22,8 @@ public class MyRouteBuilder extends RouteBuilder {
 		this.routeId = routeDef.getRouteId();
 		this.from = routeDef.getFrom();
 		this.destination = routeDef.getDestination();
-		this.log = routeDef.getLog();
-		this.tracing = routeDef.getTracing();
+		this.log = StringUtils.defaultIfEmpty(routeDef.getLog(), DEFAULT_BODY);
+		this.tracing = StringUtils.defaultIfEmpty(routeDef.getTracing(),DEFAULT_TRACING);
 	}
 
 	@Override
