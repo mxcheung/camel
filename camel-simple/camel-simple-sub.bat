79809c7e45f4e18b9c70cb577e9f680a978afaef
max cheung <maxcheung@optusnet.com.au>
2018-08-12 22:51:03 +1000

    camel exec

--- camel-simple/camel-simple-sub.bat
+++ camel-simple/camel-simple-sub.bat
@@ -1,1 +1,2 @@
-java -jar -Dspring.profiles.active=dev -Dserver.port=8089 -Dmanagement.port=8089 -Dcamelsimple.route.path=C:\camel\route\dev\sub .\target\camelsimple-0.0.1-SNAPSHOT.jar
+rem java -jar -Dspring.profiles.active=dev -Dserver.port=8089 -Dmanagement.port=8089 -Dcamelsimple.route.path=C:\camel\route\dev\sub .\target\camelsimple-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=dev -Dserver.port=8089  -Dcamelsimple.route.path=C:\camel\route\dev\sub .\target\camelsimple-0.0.1-SNAPSHOT.jar
