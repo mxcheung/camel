604e468eeaf9e8c9dc8d4eb51ac01450a20a940c
mxcheung <mxcheung@users.noreply.github.com>
2025-07-26 14:57:52 +1000

    Update MqConfig.java

--- mq-jms-spring-boot-starter/MqConfig.java
+++ mq-jms-spring-boot-starter/MqConfig.java
@@ -1,13 +1,27 @@
-import com.ibm.mq.jakarta.jms.MQConnectionFactory;
-import jakarta.jms.ConnectionFactory;
+package com.example.demo.config;
 
-@Bean
-public ConnectionFactory connectionFactory() throws JMSException {
-    MQConnectionFactory cf = new MQConnectionFactory();
-    cf.setHostName("your-host");
-    cf.setPort(1414);
-    cf.setQueueManager("QM1");
-    cf.setChannel("DEV.APP.SVRCONN");
-    cf.setTransportType(WMQConstants.WMQ_CM_CLIENT);
-    return cf;
+import com.ibm.mq.jakarta.jms.MQConnectionFactory;
+import com.ibm.msg.client.wmq.WMQConstants;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqConfig {
+
+    @Bean
+    public ConnectionFactory mqConnectionFactory() throws JMSException {
+        MQConnectionFactory cf = new MQConnectionFactory();
+
+        cf.setHostName("localhost");                  // 🔁 Your MQ host
+        cf.setPort(1414);                             // 🔁 Your MQ port
+        cf.setQueueManager("QM1");                    // 🔁 Your queue manager
+        cf.setChannel("DEV.APP.SVRCONN");             // 🔁 Your channel
+        cf.setTransportType(WMQConstants.WMQ_CM_CLIENT); // Client mode
+        cf.setStringProperty(WMQConstants.USERID, "app");
+        cf.setStringProperty(WMQConstants.PASSWORD, "app");
+
+        return cf;
+    }
 }
