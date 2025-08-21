cc9d722dd0a9827c09443c6346ea6518fd439bf1
mxcheung <mxcheung@users.noreply.github.com>
2025-08-22 09:56:54 +1000

    Update instruction_meta.java

--- instruction_metadata/instruction_meta.java
+++ instruction_metadata/instruction_meta.java
@@ -16,3 +16,13 @@ from("direct:processInstruction")
     })
     .log("Instruction metadata = ${header.instruction_metadata}")
     .to("jms:queue:instructions");
+
+from("direct:processInstruction")
+    .process(exchange -> {
+        Map<String, Object> metadata = new HashMap<>();
+        metadata.put("region", System.getenv("REGION"));
+        metadata.put("clientId", System.getenv("CLIENT_ID"));
+        exchange.getIn().setHeader("instruction_metadata", metadata);
+    })
+    .log("{\"@message\":\"Sending Message\",\"instruction_metadata\":${header.instruction_metadata}}")
+    .to("jms:queue:instructions");
