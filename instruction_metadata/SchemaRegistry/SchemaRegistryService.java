32433c85a5b00aeadabeb9ddf2b5586f7c8bd22e
mxcheung <mxcheung@users.noreply.github.com>
2025-08-23 22:35:44 +1000

    Update SchemaRegistryService.java

--- instruction_metadata/SchemaRegistry/SchemaRegistryService.java
+++ instruction_metadata/SchemaRegistry/SchemaRegistryService.java
@@ -1,34 +1,27 @@
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
-import org.apache.avro.Schema;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaMetadata;
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class SchemaRegistryService {
 
     private final SchemaRegistryClient client;
 
-    public SchemaRegistryService() {
-        this.client = new CachedSchemaRegistryClient("http://localhost:8081", 1000);
+    public SchemaRegistryService(SchemaRegistryClient client) {
+        this.client = client;
     }
 
-    @Cacheable(value = "schemas", key = "#schemaId")
-    public String getSchema(int schemaId) {
+    @Cacheable(value = "dlqSchemas", key = "#dlqTopic")
+    public SchemaMetadata getDlqSchemaMeta(String dlqTopic) {
+        String subject = dlqTopic + "-value";
         try {
-            Schema schema = client.getById(schemaId);
-            return schema.toString(true); // pretty print
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to fetch schema for id=" + schemaId, e);
-        }
-    }
-
-    public int getSchemaId(String subject, String schema) {
-        try {
-            Schema avroSchema = new Schema.Parser().parse(schema);
-            return client.register(subject, avroSchema);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to register schema for subject=" + subject, e);
+            return client.getLatestSchemaMetadata(subject);
+        } catch (IOException | RestClientException e) {
+            throw new SchemaRegistryServiceException("Failed to fetch latest schema metadata for subject=" + subject, e);
         }
     }
 }
